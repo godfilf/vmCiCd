82bbcae0331fad305924c1a48d13896e9bc60c9a
Michele <costa.tux@gmail.com>
2024-11-05 09:25:26 +0100

    Temporary commit

--- __main__.py
+++ __main__.py
@@ -11,16 +11,12 @@ import sys
 import plugin.dns_manager as dns
 import plugin.os_conn as os_conn
 import plugin.sg_manager as sg
+import plugin.volumes_manager as vols
 
 # importo tutte le var che ho creato, perchè il __main__.py era diventato illeggibile
 from plugin.globals import *
 
 #import plugin.port_manager
-
-# Var globali
-conn = os_conn.connection(auth_url, username, password, tenant)
-dns.manage_recordsets(conn, zone.id, instance_props)
-pulumi.export(f"{tenant_name}", tenant_name)
 
 # Connessione e configurazione
 conn = os_conn.connection(auth_url, username, password, tenant)
@@ -32,7 +28,7 @@ def get_config_property(vmType, prop_name, default_value):
     try:
         return instance_props[vmType].get(prop_name) or config.require(prop_name)
     except pulumi.ConfigMissingError as e:
-        if prop_name == "keyPair": return None
+        if prop_name in {"keyPair", "volumes"}: return None
         print(f"Errore: La configurazione '{prop_name}' è mancante per '{vmType}'. {e}")
         sys.exit(1)  # Termina il programma con un codice di uscita diverso da 0
 
@@ -69,8 +65,83 @@ for vmType, props in instance_props.items():
 
     # Crea un dizionario di argomenti opzionali
     optional_args = {}
-    if key_pair_name:
-        optional_args["key_pair"] = key_pair_name
+    volumes_data = get_config_property(vmType, "volumes", []) 
+    volumes = vols.volume_create({f"{instanceName}.vol-{i}.{tenant_name}": vol_data for i, vol_data in enumerate(volumes_data)}, image) if volumes_data else {}
+
+    # Configura i dispositivi per l'istanza
+    block_devices = []
+    for i, vol in enumerate(volumes.values()):
+        # Verifica se il volume è esistente usando l'UUID
+        if hasattr(vol, 'name'):
+            block_devices.append(
+                pstack.compute.InstanceBlockDeviceArgs(
+                    source_type="volume",
+                    boot_index=0 if i == 0 else 1,
+                    delete_on_termination=False,
+                    destination_type="volume",
+                    uuid=vol.id  # Utilizza l'UUID del volume esistente
+                )
+            )
+        else:
+            block_devices.append(
+                pstack.compute.InstanceBlockDeviceArgs(
+                    source_type="volume",
+                    boot_index=0 if i == 0 else 1,  # Imposta boot_index a 0 solo per il primo volume (disco di avvio)
+                    delete_on_termination=False,
+                    destination_type="volume",
+                    uuid=vol.id,
+                    volume_size=vol.size,
+                )
+            )
+    #        # Volume nuovo creato in Pulumi (disponibile in `volumes`)
+    #        vol_key = f"{instanceName}.vol-{i}.{tenant_name}"
+    #        vol = volumes.get(vol_key)
+    #        if vol:  # Assicurati che il volume sia stato creato
+    #            block_devices.append(
+    #                pstack.compute.InstanceBlockDeviceArgs(
+    #                    source_type="volume",
+    #                    boot_index=0 if i == 0 else 1,
+    #                    delete_on_termination=False,
+    #                    destination_type="volume",
+    #                    uuid=vol.id,
+    #                    volume_size=vol.size,
+    #                    volume_type=vol.volume_type
+    #                )
+    #            )
+
+
+#    block_devices = [
+#        pstack.compute.InstanceBlockDeviceArgs(
+#            source_type="volume",
+#            boot_index=0 if i == 0 else 1,  # Imposta boot_index a 0 solo per il primo volume (disco di avvio)
+#            delete_on_termination=False,
+#            destination_type="volume",
+#            uuid=vol.id,
+#            volume_size=vol.size,
+#            ##**({"volume_type": vol.volume_type} if not vol.get("existing") else {})
+#
+#            #uuid=vol["id"] if isinstance(vol, dict) and vol.get("existing") else vol.id,
+#            #volume_size=None if isinstance(vol, dict) and vol.get("existing") else vol.size,
+#
+#            #**({"volume_type": vol.volume_type} if not vol.get("existing") else {})  # Assicura che `volume_type` sia incluso solo per i nuovi volumi
+#            #**({"volume_type": vol.volume_type} if not isinstance(vol, dict) or not vol.get("existing") else {})
+#            #**({"volume_type": vol.volume_type} if not (isinstance(vol, dict) and vol.get("existing")) else {})  # Includi `volume_type` solo per volumi nuovi
+#            #**({} if isinstance(vol, dict) and vol.get("existing") else {"volume_type": vol.volume_type})
+#            #**({} if vol.get("existing") else {"volume_size": vol.size, "volume_type": vol.volume_type})
+#
+#        )
+#        for i, vol in enumerate(volumes.values())
+#    ]
+
+    optional_components = {
+        "key_pair": key_pair_name,
+        "block_devices": block_devices
+    }
+
+    # Aggiunge solo i componenti non None a optional_args
+    for key, value in optional_components.items():
+        if value is not None:
+            optional_args[key] = value
     
     server_group = sg.cd(conn, vmCount, instanceName, tenant_name)
     instances = [create_instance(instanceName, flavor, image, network, server_group, i) for i in range(vmCount)]
---
+++ plugin/volumes_manager.py
@@ -0,0 +1,33 @@
+import pulumi_openstack as pstack
+import plugin.os_conn as os_conn
+
+from plugin.globals import *
+
+def get_image_id(image_name):
+    conn = os_conn.connection(auth_url, username, password, tenant)
+    image = conn.compute.find_image(image_name)
+    if image:
+        return image.id
+    else:
+        #raise ValueError(f"Immagine '{image_name}' non trovata")
+        return None
+
+
+def volume_create(volumes, image_name=None):
+    created_volumes = {}
+
+    for i, (volume_key, volume_props) in enumerate(volumes.items()):
+        if "uuid" in volume_props:
+            created_volumes[volume_key] = volumes[volume_name] = Volume(id=volume_props["uuid"])
+        else:
+            created_volumes[volume_key] = pstack.blockstorage.Volume(
+                resource_name=volume_key,
+                region="RegionOne",
+                name=volume_key,
+                description=volume_props["description"],
+                size=volume_props["size"],
+                volume_type=volume_props.get("type", None),  # Usa 'ceph' come tipo predefinito se non specificato
+                image_id= get_image_id(image_name) if i == 0 else None
+            )
+    return created_volumes
+
